6b33b02327b0458940139bb7c108838904c21f57
AnilBurcu <anlbrc95@gmail.com>
2025-04-25 16:36:26 +0300

    Main Design

--- src/navigation/Routes.jsx
+++ src/navigation/Routes.jsx
@@ -9,18 +9,63 @@ import SongInfoScreen from '../screens/SongInfoScreen';
 import SongsScreen from '../screens/SongsScreen';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
+import {Home, Profile} from 'iconsax-react-nativejs';
 
 const {HOME, SONGINFO, SONGS, PROFILE, LOGIN} = SCREENS;
 const Tab = createBottomTabNavigator();
 
 const BottomTabs = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name={HOME} component={HomeScreen} />
-      <Tab.Screen name={LOGIN} component={LoginScreen} />
-      <Tab.Screen name={PROFILE} component={ProfileScreen} />
-      <Tab.Screen name={SONGINFO} component={SongInfoScreen} />
-      <Tab.Screen name={SONGS} component={SongsScreen} />
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: '#131624',
+          shadowOpacity: 0.3,
+          shadowRadius: 4,
+          shadowOffset: {
+            width: 0,
+            height: -10,
+          },
+          position: 'absolute',
+          bottom: 0,
+          left: 0,
+          right: 0,
+          height: 85,
+          borderWidth: 0,
+        },
+      }}>
+      <Tab.Screen
+        name={HOME}
+        component={HomeScreen}
+        options={{
+          headerShown: false,
+          tabBarLabel: 'Home',
+          tabBarLabelPosition: 'beside-icon',
+          tabBarLabelStyle: {color: 'white', fontSize: 13, fontWeight: '600'},
+          tabBarIcon: ({focused}) =>
+            focused ? (
+              <Home size={32} color="#65d46e" />
+            ) : (
+              <Home size={22} color="white" />
+            ),
+        }}
+      />
+      <Tab.Screen
+        name={PROFILE}
+        component={ProfileScreen}
+        options={{
+          headerShown: false,
+          tabBarLabel: 'Profile',
+          tabBarLabelPosition: 'beside-icon',
+          tabBarLabelStyle: {color: 'white', fontSize: 13, fontWeight: '600'},
+          tabBarIcon: ({focused}) =>
+            focused ? (
+              <Profile size={32} color="#65d46e" />
+            ) : (
+              <Profile size={22} color="white" />
+            ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,14 +1,92 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  View,
+  Pressable,
+  TouchableOpacity,
+} from 'react-native';
 import React from 'react';
+import LinearGradient from 'react-native-linear-gradient';
+import {Call, Facebook, Google, Mobile, Spotify} from 'iconsax-react-nativejs';
 
-const LoginScreen = () => {
+const LoginScreen = ({navigation}) => {
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
+    <LinearGradient colors={['#040306', '#131624']} style={{flex: 1}}>
+      <SafeAreaView style={{flex: 1}}>
+        <View style={{height: 80}} />
+        <View style={{alignItems: 'center'}}>
+          <Spotify color="#65d46e" size={96} />
+        </View>
+
+        <Text style={styles.loginTitle}>Millions of songs free on Spotify</Text>
+        <View style={{height: 80}} />
+        <TouchableOpacity
+          style={styles.loginButton}
+          onPress={() => navigation.navigate('Main')}>
+          <Text style={styles.buttonTextSpt}>Sign In with Spotify!</Text>
+        </TouchableOpacity>
+        <Pressable style={styles.button}>
+          <Google color="white" size={24} />
+          <Text style={styles.buttonText}>Continue with Google!</Text>
+        </Pressable>
+        <Pressable style={styles.button}>
+          <Facebook color="white" size={24} />
+          <Text style={styles.buttonText}>Continue with Facebook!</Text>
+        </Pressable>
+        <Pressable style={styles.button}>
+          <Call color="white" size={24} />
+          <Text style={styles.buttonText}>Continue with Phone!</Text>
+        </Pressable>
+      </SafeAreaView>
+    </LinearGradient>
   );
 };
 
 export default LoginScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  loginTitle: {
+    color: 'white',
+    fontSize: 40,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 30,
+  },
+  loginButton: {
+    backgroundColor: '#65d46e',
+    padding: 10,
+    width: 300,
+    alignSelf: 'center',
+    marginVertical: 10,
+    borderRadius: 25,
+    height: 40,
+  },
+  buttonTextSpt: {
+    color: 'white',
+    marginLeft: 64,
+    fontWeight: '600',
+    fontSize: 15,
+    flex: 1,
+  },
+  buttonText: {
+    color: 'white',
+    marginLeft: 40,
+    fontWeight: '600',
+    fontSize: 15,
+    flex: 1,
+  },
+  button: {
+    backgroundColor: '#131624',
+    padding: 10,
+    marginHorizontal: 'auto',
+    flexDirection: 'row',
+    borderColor: '#C0C0C0',
+    width: 300,
+    height: 40,
+    borderRadius: 25,
+    alignSelf: 'center',
+    borderWidth: 0.8,
+    marginVertical: 10,
+  },
+});
